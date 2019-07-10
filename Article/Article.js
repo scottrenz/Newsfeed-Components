77b8161c99cf4610b5ff32e00b6f70404e3924b7
Scott Renz <scottrenz@scottrenz.com>
2019-07-10 19:49:05 -0400

    Newsfeed got funtion to add articl

--- Article/Article.js
+++ Article/Article.js
@@ -1,4 +1,10 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+
+let article = document.querySelectorAll('.articles');
+
+  article[0].appendChild(
+  newArticle(2,'New Article', 'Jul 10th, 2019',['line 1','line 2'])
+  )
  
 class Article {
   constructor(domElement) {
@@ -40,7 +46,8 @@ function newArticle(numofParagraphs,title,date,text) {
   const divArticle = document.createElement('div');
   const h2 = document.createElement('h2');
   const pDate = document.createElement('p');
-for (i=0, p=[]; i<numofParagraphs; i++)
+  const p = [];
+  for (i=0; i<numofParagraphs; i++)
 { p[i] = document.createElement('p')
 p[i].innerHTML = text[i]
 }
@@ -54,17 +61,14 @@ pDate.innerHTML = date;
 divArticles.appendChild(divArticle)
 divArticle.appendChild(h2)
 divArticle.appendChild(pDate)
-for (i=0, p=[]; i<numofParagraphs; i++)
+for (i=0; i<numofParagraphs; i++)
 {
+  divArticle.appendChild(p[i])
 }
 divArticle.appendChild(expandButton)
-document.body.appendChild(divArticles);
+// document.body.appendChild(divArticles);
 
 
 return divArticle
 }
-
-  // articles.appendChild(
-  newArticle(2,'New Article', 'Jul 10th, 2019',['line 1','line 2'])
-  
 
