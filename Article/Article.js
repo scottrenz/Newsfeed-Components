96338f5bca70e4dbeb9f6fcf0b8dc139ca74cb06
Scott Renz <scottrenz@scottrenz.com>
2019-07-10 15:31:46 -0400

    Newsfeed first commit second round

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,5 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
+ 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
